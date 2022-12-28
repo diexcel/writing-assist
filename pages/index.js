7fe4f0c94d3a96724b4fcc2e782b486077d2b399
Daniel Lee <danielee1991@gmail.com>
2022-12-27 19:18:47 -0800

    added User input handling & Generate Button

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -17,7 +26,19 @@ const Home = () => {
             <h2>insert your subtitle here</h2>
           </div>
           <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
+            <textarea 
+              className="prompt-box"   
+              placeholder="start typing here" 
+              value={userInput}
+              onChange={onUserChangedText}
+            />
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={null}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
         </div>
         </div>
       </div>
